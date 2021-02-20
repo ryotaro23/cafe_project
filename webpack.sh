c5a44f2afae0d9997f89dde825bfac3a0fc8ad3b
marumaru1019 <ryu.ryusei401@gmail.com>
2021-02-21 01:43:18 +0900

    ci動作確認

--- webpack.sh
+++ webpack.sh
@@ -1,5 +1,9 @@
 # webpackのビルド
 bin/webpack
 
+# Rails: Vanilla Rails 6.0 error Command “webpack” not found
+# precompileをしたあとはする必要がある
+# rails webpacker:install
+
 # webpackの動作確認　ビルドじゃないから反映されない
 bin/webpack-dev-server
