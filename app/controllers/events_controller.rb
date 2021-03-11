aeca69cfed1a146a69ddf4c28f7777ec97cc5e25
marumaru1019 <ryu.ryusei401@gmail.com>
2021-03-11 21:37:57 +0900

    CA-3: create event_join table and validation setting

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,10 +3,11 @@ class EventsController < InheritedResources::Base
   def top
     @events = Event.order(created_at: :desc).limit(5)
   end
+
   private
 
-    def event_params
-      params.require(:event).permit(:name, :time_id, :recommend_menu, :place, :max_num, :image)
-    end
+  def event_params
+    params.require(:event).permit(:name, :time_id, :recommend_menu, :place, :max_num, :image)
+  end
 
 end
