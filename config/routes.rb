e2b3d6ec176e54ea8a2b0c3e80832d5dc5834a10
marumaru1019 <ryu.ryusei401@gmail.com>
2021-03-12 02:01:56 +0900

    CA-4: マイページの作製が完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
   resources :events, only: [:show, :index] do
     # topページへのルーティングを追加
     collection do
@@ -12,12 +13,16 @@ Rails.application.routes.draw do
   # get 'event/top', to: 'event#top'
 
   devise_for :users
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   root  'events#index'  #この1行を追加
 
+  resources :users, only: [:show, :edit, :update]
 
   get 'hello/index', to: 'hello#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'hello/create', to: 'hello#create'
+
 end
