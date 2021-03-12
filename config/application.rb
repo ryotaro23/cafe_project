fd7ad9f1eb11c59aae19f51cbf7ae54322419a13
marumaru1019 <ryu.ryusei401@gmail.com>
2021-03-13 02:14:29 +0900

    CA-6: introduce i18n and config valiable

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # TimeZone
+    config.time_zone = "Tokyo"
+    # config.active_record.default_timezone = :local
+
+    # Language
+    config.i18n.default_locale = :ja
+    # config.i18n.available_locales = %i(ja en zh ko vi)
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
