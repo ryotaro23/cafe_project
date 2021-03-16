498b2d70d5762a60e5e523101708bd7e5311f58c
marumaru1019 <ryu.ryusei401@gmail.com>
2021-03-16 23:15:43 +0900

    CA-11: create top and greeting site test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     name { Faker::Lorem.characters(number: 10) }
     email { Faker::Internet.email }
     university { Faker::Lorem.characters(number: 10) }
-    university { Faker::Lorem.characters(number: 10) }
     password { 'password' }
     password_confirmation { 'password' }
     grade { 1 }
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -89,4 +89,14 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # handling Devise::MissingWarden error
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
+  config.include Devise::Test::ControllerHelpers, type: :view
+
+  # to use factory in rspec
+  RSpec.configure do |config|
+    config.include FactoryBot::Syntax::Methods
+  end
 end
