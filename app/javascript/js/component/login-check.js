6c02bd703615eceb2e85be1ee7411b7c41a2949d
marumaru1019 <ryu.ryusei401@gmail.com>
2021-03-17 03:41:03 +0900

    CA-14: add form focus event

--- app/javascript/js/component/login-check.js
+++ app/javascript/js/component/login-check.js
@@ -5,10 +5,32 @@ document.addEventListener("turbolinks:load", function () {
     let loginPasswordButton = document.getElementsByClassName('js-login-password')[0];
     loginSubmitButton.disabled = true;
 
+    // focus and blur event (blur: out of focus)
+    loginEmailButton.addEventListener('focus', function () {
+        console.log("fucus");
+        $("#js-login-title").css("color", "red");
+    });
 
+    loginEmailButton.addEventListener('blur', function () {
+        console.log("fucus");
+        $("#js-login-title").css("color", "black");
+    });
+
+    loginPasswordButton.addEventListener('focus', function () {
+        console.log("fucus");
+        $("#js-login-title").css("color", "red");
+    });
+
+    loginPasswordButton.addEventListener('blur', function () {
+        console.log("fucus");
+        $("#js-login-title").css("color", "black");
+    });
+
+    // validation check
     loginEmailButton.addEventListener('keyup', function () {
         console.log(this.value.length);
         console.log(this.value);
+        // email string has @?
         if (this.value.length < 2 || !this.value.match(/@/)) {
             loginSubmitButton.disabled = true;
         } else {
