80f8f0f7dfb4cfce4991c1873d8cb831826dbfdc
marumaru1019 <ryu.ryusei401@gmail.com>
2021-03-18 02:23:04 +0900

    CA-19: create registration form and few tests

--- app/javascript/js/component/login-check.js
+++ app/javascript/js/component/login-check.js
@@ -3,48 +3,44 @@ document.addEventListener("turbolinks:load", function () {
     let loginSubmitButton = document.getElementsByClassName('js-login-submit')[0];
     let loginEmailButton = document.getElementsByClassName('js-login-email')[0];
     let loginPasswordButton = document.getElementsByClassName('js-login-password')[0];
-    loginSubmitButton.disabled = true;
+    // loginSubmitButton.disabled = true;
 
     // focus and blur event (blur: out of focus)
     loginEmailButton.addEventListener('focus', function () {
-        console.log("fucus");
-        $("#js-login-title").css("color", "red");
+        $("#js-login-title").css("color", "#0886e0");
     });
 
     loginEmailButton.addEventListener('blur', function () {
-        console.log("fucus");
         $("#js-login-title").css("color", "black");
     });
 
     loginPasswordButton.addEventListener('focus', function () {
-        console.log("fucus");
-        $("#js-login-title").css("color", "red");
+        $("#js-login-title").css("color", "#0886e0");
     });
 
     loginPasswordButton.addEventListener('blur', function () {
-        console.log("fucus");
         $("#js-login-title").css("color", "black");
     });
 
     // validation check
-    loginEmailButton.addEventListener('keyup', function () {
-        console.log(this.value.length);
-        console.log(this.value);
-        // email string has @?
-        if (this.value.length < 2 || !this.value.match(/@/)) {
-            loginSubmitButton.disabled = true;
-        } else {
-            loginSubmitButton.disabled = false;
-        }
-    }, false);
+    // loginEmailButton.addEventListener('keyup', function () {
+    //     console.log(this.value.length);
+    //     console.log(this.value);
+    //     // email string has @?
+    //     if (this.value.length < 2 || !this.value.match(/@/)) {
+    //         loginSubmitButton.disabled = true;
+    //     } else {
+    //         loginSubmitButton.disabled = false;
+    //     }
+    // }, false);
 
-    loginPasswordButton.addEventListener('keyup', function () {
-        console.log(this.value.length);
-        if (this.value.length < 6) {
-            loginSubmitButton.disabled = true;
-        } else {
-            loginSubmitButton.disabled = false;
-        }
-    }, false);
+    // loginPasswordButton.addEventListener('keyup', function () {
+    //     console.log(this.value.length);
+    //     if (this.value.length < 6) {
+    //         loginSubmitButton.disabled = true;
+    //     } else {
+    //         loginSubmitButton.disabled = false;
+    //     }
+    // }, false);
 
 });
