b8905201437f0437084127ac8c2369872fc83068
marumaru1019 <ryu.ryusei401@gmail.com>
2021-03-23 21:28:00 +0900

    CA-26: add day format

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,13 @@ class Event < ApplicationRecord
     has_many :event_joins, dependent: :destroy
     belongs_to :time_table, foreign_key: "time_id"
 
+    # concernのput_time.rbを呼びだす
+    include PutTime
+
+
     def join_by?(user)
         event_joins.where(user_id: user.id).exists?
     end
+
+
 end
