50125bc6ac8d2f7b9d9e9e5f974628b4408ffed4
marumaru1019 <ryu.ryusei401@gmail.com>
2021-03-29 04:48:30 +0900

    CA-24: create_event_detail

--- spec/system/authenticated/02_after_login_spec.rb
+++ spec/system/authenticated/02_after_login_spec.rb
@@ -1,147 +1,192 @@
 require 'rails_helper'
 
 describe 'ユーザログイン後のテスト' do
+
     let(:user) { create(:user) }
-    # let!(:other_user) { create(:user) }
     let(:event) { create(:event) }
-    # let!(:book) { create(:book, user: user) }
+    let(:event_3in) { create(:event, date: Date.today+1, max_num: 10) }
+    let(:event_3out) { create(:event, date: Date.today+5, max_num: 10) }
+    let(:event_out) { create(:event, date: Date.today-3, max_num: 10) }
+    let(:event_max_1) { create(:event, max_num: 1) }
 
-        before do
-            visit user_session_path
-            fill_in 'user[email]', with: user.email
-            fill_in 'user[password]', with: user.password
-            click_button 'ログイン'
-        end
+    before do
+        # 開始3日以内, 開始3日以前, 終了済みの3パターンで分類
+        visit user_session_path
+        fill_in 'user[email]', with: user.email
+        fill_in 'user[password]', with: user.password
+        click_button 'ログイン'
+    end
 
-        describe 'ヘッダーのテスト: ログインしている場合' do
-            context 'リンクの内容を確認: ※logoutは『ユーザログアウトのテスト』でテスト済みになります。' do
-                subject { current_path }
+    describe 'ヘッダーのテスト: ログインしている場合' do
+        context 'リンクの内容を確認: ※logoutは『ユーザログアウトのテスト』でテスト済みになります。' do
+            subject { current_path }
 
-                it 'ヘッダーロゴのルーティング' do
-                    logo = find_by_id('header-logo')
-                    logo.click
-                    is_expected.to eq '/'
-                end
+            before do
+                visit root_path
+            end
 
-                it '代表者挨拶のルーティング' do
-                    click_on '代表者挨拶'
-                    is_expected.to eq '/events/greeting'
-                end
+            it 'ヘッダーロゴのルーティング' do
+                # expect(page).to have_content '間違っています'
+                logo = find_by_id('header-logo')
+                logo.click
+                is_expected.to eq '/'
+            end
 
-                it 'マイページのルーティング' do
-                    click_on 'マイページ'
-                    is_expected.to eq '/users/' + user.id.to_s
-                end
+            it '代表者挨拶のルーティング' do
+                click_on '代表者挨拶'
+                is_expected.to eq '/events/greeting'
+            end
 
-                it 'ヘッダーに新規登録とログインがないことを確認する' do
-                    expect(page).to have_no_xpath '//a[contains(text(), "新規登録")]'
-                    expect(page).to have_no_xpath '//a[contains(text(), "新規登録")]'
-                end
+            it 'マイページのルーティング' do
+                click_on 'マイページ'
+                is_expected.to eq '/users/' + user.id.to_s
+            end
+
+            it 'ヘッダーに新規登録とログインがないことを確認する' do
+                expect(page).to have_no_xpath '//a[contains(text(), "新規登録")]'
+                expect(page).to have_no_xpath '//a[contains(text(), "新規登録")]'
             end
         end
+    end
 
-    describe 'トップページの確認' do
+    describe 'イベント一覧画面のテスト' do
 
         before do
-            visit root_path
+            create(:event, name: "sample1")
+            visit events_path
         end
 
         context '表示内容の確認' do
-
             it 'URLが正しい' do
-                expect(current_path).to eq '/'
+                expect(current_path).to eq '/events'
             end
-
-            # it '自分と他人の画像のリンク先が正しい' do
-            #     expect(page).to have_link '', href: user_path(book.user)
-            #     expect(page).to have_link '', href: user_path(other_book.user)
-            # end
-
-            # it '自分の投稿と他人の投稿のタイトルのリンク先がそれぞれ正しい' do
-            #     expect(page).to have_link book.title, href: book_path(book)
-            #     expect(page).to have_link other_book.title, href: book_path(other_book)
-            # end
-
-            # it '自分の投稿と他人の投稿のオピニオンが表示される' do
-            #     expect(page).to have_content book.body
-            #     expect(page).to have_content other_book.body
-            # end
-
-
+            it '「イベント一覧」と表示される' do
+                expect(page).to have_content 'イベント一覧'
+            end
+            it 'イベントの名前が表示される' do
+                expect(page).to have_content "sample1"
+            end
+            it 'イベントの名前をクリックして詳細画面に飛べる' do
+                click_on 'sample1'
+                expect(page).to have_content "sample1"
+            end
         end
-
-#     context 'サイドバーの確認' do
-#       it '自分の名前と紹介文が表示される' do
-#         expect(page).to have_content user.name
-#         expect(page).to have_content user.introduction
-#       end
-#       it '自分のユーザ編集画面へのリンクが存在する' do
-#         expect(page).to have_link '', href: edit_user_path(user)
-#       end
-#       it '「New book」と表示される' do
-#         expect(page).to have_content 'New book'
-#       end
-#       it 'titleフォームが表示される' do
-#         expect(page).to have_field 'book[title]'
-#       end
-#       it 'titleフォームに値が入っていない' do
-#         expect(find_field('book[title]').text).to be_blank
-#       end
-#       it 'opinionフォームが表示される' do
-#         expect(page).to have_field 'book[body]'
-#       end
-#       it 'opinionフォームに値が入っていない' do
-#         expect(find_field('book[body]').text).to be_blank
-#       end
-#       it 'Create Bookボタンが表示される' do
-#         expect(page).to have_button 'Create Book'
-#       end
-#     end
-
-#     context '投稿成功のテスト' do
-#       before do
-#         fill_in 'book[title]', with: Faker::Lorem.characters(number: 5)
-#         fill_in 'book[body]', with: Faker::Lorem.characters(number: 20)
-#       end
-
-#       it '自分の新しい投稿が正しく保存される' do
-#         expect { click_button 'Create Book' }.to change(user.books, :count).by(1)
-#       end
-#       it 'リダイレクト先が、保存できた投稿の詳細画面になっている' do
-#         click_button 'Create Book'
-#         expect(current_path).to eq '/books/' + Book.last.id.to_s
-#       end
-#     end
     end
 
-    # describe '自分の投稿詳細画面のテスト' do
-    #     before do
-    #     visit book_path(book)
-    #     end
+    describe '1週間以内のイベント' do
 
-    #     context '表示内容の確認' do
-    #         it 'URLが正しい' do
-    #             expect(current_path).to eq '/books/' + book.id.to_s
-    #         end
-    #         it '「Book detail」と表示される' do
-    #             expect(page).to have_content 'Book detail'
-    #         end
-    #         it 'ユーザ画像・名前のリンク先が正しい' do
-    #             expect(page).to have_link book.user.name, href: user_path(book.user)
-    #         end
-    #         it '投稿のtitleが表示される' do
-    #             expect(page).to have_content book.title
-    #         end
-    #         it '投稿のopinionが表示される' do
-    #             expect(page).to have_content book.body
-    #         end
-    #         it '投稿の編集リンクが表示される' do
-    #             expect(page).to have_link 'Edit', href: edit_book_path(book)
-    #         end
-    #         it '投稿の削除リンクが表示される' do
-    #             expect(page).to have_link 'Destroy', href: book_path(book)
-    #         end
-    #     end
+        before do
+            create(:event, name: "sample2",date: Date.today+1 )         #0: 一週間以内
+            create(:event, name: "sample3",date: Date.today+10 )        #1: 一週間以上後
+            create(:event, name: "sample4",date: Date.today-1 )         #2: 経過後
+            visit events_path
+        end
+
+        context '表示内容の確認' do
+            it '一週間以内のイベントであることが表示されている' do
+                whithin7Day = all(".event-all__menu__item__content")[0]
+                expect(whithin7Day).to have_content "1週間以内のイベント！！"
+            end
+            it 'イベントの名前をクリックして詳細画面に飛べる' do
+                click_on 'sample2'
+                expect(page).to have_content "sample2"
+            end
+            it '一週間以内のイベントであることが表示されている' do
+                more7Day = all(".event-all__menu__item__content")[1]
+                expect(more7Day).to have_no_content "1週間以内のイベント！！"
+                expect(more7Day).to have_no_content "開催済み"
+            end
+            it '一週間以内のイベントであることが表示されている' do
+                afterEvent = all(".event-all__menu__item__content")[2]
+                expect(afterEvent).to have_content "開催済み"
+            end
+        end
+
+    end
+
+    describe '3日以内のイベントのテスト' do
+        before do
+            visit event_path(event_3in)
+        end
+
+        context '3日以内のイベントのテスト' do
+            it 'URLが正しい' do
+                expect(current_path).to eq '/events/' + event_3in.id.to_s
+            end
+            it '参加可能の表示がされる' do
+                expect(page).to have_content "参加可能"
+            end
+            it '参加するボタンを押すと参加人数が増える' do
+                expect(page).to have_content "0/"
+                expect(page).to have_content "参加する"
+                click_on '参加する'
+                expect(page).to have_content "1/"
+            end
+            it '一度参加ボタンを押すとキャンセル不可にできる' do
+                click_on '参加する'
+                expect(page).to have_content "キャンセル不可"
+            end
+            it 'cssを変更してキャンセル不可を押してもroot_pathへ飛ばす' do
+                click_on '参加する'
+                expect(page).to have_content "キャンセル不可"
+                click_on 'キャンセル不可'
+                expect(current_path).to eq root_path
+            end
+        end
+    end
+
+    describe '3日以上空いているときのテスト' do
+        before do
+            visit event_path(event_3out)
+        end
+
+        context '3日以上空いているときのテスト' do
+            it 'URLが正しい' do
+                expect(current_path).to eq '/events/' + event_3out.id.to_s
+            end
+            it '参加可能の表示がされる' do
+                expect(page).to have_content "参加可能"
+            end
+            it '参加するボタンを押すと参加人数が増える' do
+                expect(page).to have_content "0/"
+                expect(page).to have_content "参加する"
+                click_on '参加する'
+                expect(page).to have_content "1/"
+            end
+            it '参加すると参加取り消しボタンに変更できる' do
+                click_on '参加する'
+                expect(page).to have_content "参加取り消し"
+            end
+            it '参加取り消しをすると人数を減らせる' do
+                click_on '参加する'
+                expect(page).to have_content "1/"
+                expect(page).to have_content "参加取り消し"
+                click_on '参加取り消し'
+                expect(page).to have_content "0/"
+            end
+        end
+    end
+
+    describe '終了済みのイベントのテスト' do
+        before do
+            visit event_path(event_out)
+        end
+
+        context '終了済みのイベントのテスト' do
+            it 'URLが正しい' do
+                expect(current_path).to eq '/events/' + event_out.id.to_s
+            end
+            it '終了済みボタンが表示される' do
+                expect(page).to have_content "終了済み"
+            end
+            it 'cssを変更して終了済みを押されてもroot_pathに飛ばされる' do
+                click_on '終了済み'
+                expect(current_path).to eq root_path
+            end
+        end
+    end
+
+end
 
 #     context 'サイドバーの確認' do
 #       it '自分の名前と紹介文が表示される' do
@@ -402,4 +447,3 @@ describe 'ユーザログイン後のテスト' do
 #       end
 #     end
 #   end
-end
